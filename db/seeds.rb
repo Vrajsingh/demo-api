1b86ed95782e4143d08c4507ea8fa7e7053c5d97
VISHAL RAJ <vishalrajput97084@gmail.com>
2022-09-26 17:45:43 +0530

    first init - Api dev

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+
+Product.create([
+    { name: "A1", brand: "abc", price: "200$" }, 
+    { name: "B1", brand: "abc", price: "201$" }, 
+    { name: "C1", brand: "abc", price: "202$" }, 
+    { name: "D1", brand: "abc", price: "203$" }, 
+    { name: "E1", brand: "abc", price: "204$" }, 
+    { name: "F1", brand: "abc", price: "205$" }, 
+])
